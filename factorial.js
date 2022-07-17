a00cf9e0b24ea5d295a15484b85d75a43c34a4bd
Dinusha Naveen <dinusha.dasanayaka@keeneye.solutions>
2022-07-17 13:34:44 +0530

    Code refactoring for factorial and fibonacci

--- factorial.js
+++ factorial.js
@@ -1,24 +1,24 @@
-// Get the number for factorial
+// Get the number for factorial from command line arguments.
 const number = process.argv[2];
 
 // Calculate the factorial
-function factorial(n) {
-  if (n == 1 || n == 0) {
+function factorial(number) {
+  if (number == 1 || number == 0) {
     return 1;
   } else {
     let factorial = 1;
-    for (let i = 1; i <= n; i++) {
-      factorial = factorial * i;
+    for (let multiplier = 1; multiplier <= number; multiplier++) {
+      factorial = factorial * multiplier;
     }
     return factorial;
   }
 }
 
-// Pass the number as a terminal argument
+// Call factorial function with "number".
 console.log(factorial(number));
 
 /**
-Example commands for execution
+Example commands for execution.
 
 > node factorial.js 5
 
--- fibonacci.js
+++ fibonacci.js
@@ -1,57 +1,57 @@
-// Get which function to use
+// Get which function to use from command line arguments.
 const functionSelect = process.argv[2];
-// Get how many numbers requires to be in the output
+// Get how many numbers requires to be in the output from command line arguments.
 const number = process.argv[3];
 
-// Fibinacci sequence with if-else statements
-function fibonacci_with_if_else(n) {
+// Generate fibinacci sequence with if-else statements.
+function fibonacci_with_if_else(number) {
   let series = [0, 1];
-  if (n === 1) {
+  if (number === 1) {
     return [0];
-  } else if (n === 2) {
+  } else if (number === 2) {
     return series;
   } else {
-    for (let i = 2; i < n; i++) {
-      series[i] = series[i - 1] + series[i - 2];
+    for (let index = 2; index < number; index++) {
+      series[index] = series[index - 1] + series[index - 2];
     }
     return series;
   }
 }
 
-// Fibinacci sequence with switch-case statements
-function fibonacci_with_switch_case(n) {
+// Generate fibinacci sequence with switch-case statements.
+function fibonacci_with_switch_case(number) {
   let series = [0, 1];
 
-  switch (n) {
-    case (n===1):
+  switch (number) {
+    case (number === 1):
       return [0];
       break;
 
-    case (n===2):
+    case (number === 2):
       return series;
       break;
 
     default:
-      for (let i = 2; i < n; i++) {
-        series[i] = series[i - 1] + series[i - 2];
+      for (let index = 2; index < number; index++) {
+        series[index] = series[index - 1] + series[index - 2];
       }
       return series;
       break;
   }
 }
 
-// Pass 1 to selest if-else statements function
+// Pass 1 to selest if-else statements function.
 if (functionSelect == 1) {
   console.log(fibonacci_with_if_else(number));
 }
 
-// Pass 2 to selest switch-case statements function
+// Pass 2 to selest switch-case statements function.
 if (functionSelect == 2) {
   console.log(fibonacci_with_switch_case(number));
 }
 
 /**
-Example commands for execution
+Example commands for execution.
 
 > node fibonacci.js 1 5
 > node fibonacci.js 2 8
