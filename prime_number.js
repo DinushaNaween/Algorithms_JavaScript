ea790d5a269f31994828f9ad2a866ac994dc72c8
Dinusha Naveen <dinusha.dasanayaka@keeneye.solutions>
2022-07-17 13:45:46 +0530

    Optimize prime number function

--- prime_number.js
+++ prime_number.js
@@ -7,7 +7,7 @@ function primeNumber(number) {
     return "Not a prime number.";
   } else {
     let factorCounter = 2;
-    for (let devider = 2; devider < number; devider++) {
+    for (let devider = 2; devider <= Math.sqrt(number); devider++) {
       if (number % devider === 0) {
         return "Not a prime number";
       }
@@ -24,6 +24,14 @@ Example commands for execution.
 
 > node prime_number.js 5
 
-Time complexity = O(n)
+Time complexity
+
+if we use ` devider < number ` code snippet in line 10,
+time complexity will be O(n)
+because the number if executions will increse with the number itself.
+
+but if we use ` devider <= Math.sqrt(number) ` in the line 10,
+time complexity will be O(sqrt(n))
+because the number if executions will decrese with the number itself.
 
 **/
